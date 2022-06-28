323f4955606b2152d5628fc32a1c2ea83f3953d6
niole <niolenelson@gmail.com>
2022-06-28 15:03:16 -0700

    Add in weekly percent increase

--- weeklymiles.py
+++ weeklymiles.py
@@ -4,10 +4,16 @@ parser = argparse.ArgumentParser(description='Get the rolling weekly mileage ave
 parser.add_argument('--avgs', type=int, nargs='+',
                     help='Get the rolling weekly average per day given a log of previous mileage')
 
+parser.add_argument('--prcnt', dest='prcnt', action='store_const',
+                    const=True, default=False, help='Whether or not to show the rolling percent increase over last week')
+
 args = parser.parse_args()
+
+print(args.prcnt)
 
 avgs = args.avgs
 aggregated = [0 for _ in range(len(avgs))]
+percent_increase = [0 for _ in range(len(avgs))]
 if len(avgs) >= 1:
     aggregated[0] = avgs[0]
 
@@ -17,4 +23,10 @@ if len(avgs) > 1:
         if i >= 7:
             aggregated[i] -= avgs[i-7]
 
-print(aggregated)
+        if i >= 14:
+            percent_increase[i] = ((aggregated[i] - aggregated[i-7])/float(aggregated[i-7]))*100
+
+if args.prcnt:
+    print(zip(aggregated, percent_increase))
+else:
+    print(aggregated)
